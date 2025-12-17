c81b82989d1034df9e9b8cefafc6e7fcf1adb0f2
Alex-Cristian <craciunescua5@gmail.com>
2025-12-17 23:39:33 +0200

    Fix meniu

--- meniuri_qr_comanda/urls.py
+++ meniuri_qr_comanda/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("demo.urls"))
+    path('meniu/', include("demo.urls"))
 ]
 
 if settings.DEBUG:
