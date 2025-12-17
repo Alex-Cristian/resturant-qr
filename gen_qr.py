a007c160aa778fb2c582e800ee080c828ceaf8d2
Alex-Cristian <craciunescua5@gmail.com>
2025-12-17 23:47:33 +0200

    Fix meniu

--- gen_qr.py
+++ gen_qr.py
@@ -1,8 +1,8 @@
 import qrcode
 
-base_url = "https://resturant-qr.onrender.com/meniu/masa"
+base_url = "http://https://resturant-qr.onrender.com/"
 
 for masa in range(1,11):
-    url = f"{base_url}/{masa}"
+    url = f"{base_url}?masa={masa}"
     img = qrcode.make(url)
     img.save(f"qr_codes/masa_{masa}.png")
